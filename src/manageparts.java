43e93c1d839f7fe68153a5add8112850a3967170
PVelez26 <rootslion@gmail.com>
2017-02-18 14:27:09 -0500

    Update manageparts.java

--- src/manageparts.java
+++ src/manageparts.java
@@ -63,6 +63,25 @@ public class Part {
 			this.partSellPrice = partSellPrice;
 		}
 		
+		
+		//Create a toString method that will return a String object that contains the value of each data member. 
+		//Use the @Override annotation. Display the class of the object using the getClass method.
+		//Call the toString method in the superclass to display the Part data.
+		
+		public String toString() {
+			return "Part ID: " + partID + this.partID + "Part Description: " + this.partDescription + "Part Selling Price: "
+					+ this.partSellPrice;
+		}
+			
+			@Override
+			  public String toString() {
+			    return "["+getPart()+"]";
+			    
+			    
+			
+		}
+			
+		}
 ////////////////////////////////////////////////////////////////////////////////////////
 }
 
@@ -94,7 +113,7 @@ The following requirements are for the PurchasedPart class.
 Make this class a subclass of the Part class.
 
 Create the three constructors described in the UML class diagram. The first two constructors should use 'this' to call the third constructor (the constructor with six parameters). Use a combination of arguments and default values for the necessary arguments. The third constructor should use "super" to call the superclass constructor passing the appropriate arguments. Create edits to ensure the purchase price and the handling cost cannot be negative. Use the DEFAULT_PURCHASE_PRICE and DEFAULT_HANDLING_COST constants if the parameter values are invalid. Use the DEFAULT_VENDOR_NAME constant if a vendor name is not provided (it is null or has a length of zero). Use the public default constants from the Part class as needed. Implement the DRY principle by calling the set methods from the overloaded constructor (the one with three parameters).
-Create a toString method that will return a String object that contains the value of each data member. Use the @Override annotation. Display the class of the object using the getClass method. Call the toString method in the superclass to display the Part data.
+
 Create get and set methods for each of the 3 private data variables. The handling cost must be greater than or equal to zero. The purchase price must be greater than or equal to zero. The vendor cannot be null and must have a length greater than zero. Do not update the private variables if the new values are not valid.
 Create a public method named getTotalCost that returns the total cost from the super class plus the handling cost plus the purchase price. Use the @Override annotation to insure this method overrides the method of the same name in the parent class.
 Do not create additional data members or methods.
